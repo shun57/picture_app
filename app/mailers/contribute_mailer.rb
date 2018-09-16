80c2fb9a8941591e430fb5758bbad6c6396d8663
桜井俊介 <tuktuk.032@gmail.com>
2018-09-16 11:01:24 +0900

    メール件名変更

--- app/mailers/contribute_mailer.rb
+++ app/mailers/contribute_mailer.rb
@@ -2,6 +2,6 @@ class ContributeMailer < ApplicationMailer
   def contribute_mail(contribute)
     @contribute = contribute
     @mail = contribute.user.email
-    mail to: @mail,subject:"Snap投稿完了メール"
+    mail to: @mail,subject:"Snap投稿完了のお知らせ"
   end
 end
